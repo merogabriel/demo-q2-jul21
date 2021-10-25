bcefa29acf53fd33310f3361f6b0a78e323abc8a
Gabriel Araújo <gabriel@kenzie.com.br>
2021-10-25 10:46:51 -0300

    fix: base project cra

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import Button from "./components/Button";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello World!</header>
-      <Button text="Clique aqui"></Button>
-      <Button text="Clique aqui (alternativo)"></Button>
+      <header className="App-header">Projeto base</header>
     </div>
   );
 }
